2ec562e4d1a772ae7b04b3dbf23421571c61aec3
cmdeveloped <cmwebdevs@gmail.com>
2017-09-12 19:54:02 -0500

    Implement new jquery host and add nav animation for scroll

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  console.log('hey');
+
+  // navigation scroll to section
+  $("#toWork").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#work").offset().top
+    }, 2000);
+  });
+  $("#toAbout").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+  });
+  $("#toContact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+  });
 })

2b78e13c058fa5489762f0934af234e17235a551
cmdeveloped <cmwebdevs@gmail.com>
2017-09-13 13:43:52 -0500

    Fixed toolbar

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+// you're doing it!
+  if ($(window).scrollTop() > 50 ) {
+  $('.nav').css({'position' : 'fixed', 'top' : 0});
+  } else {
+  $('.nav').css({'position' : 'relative', 'top' : 'none'});
+  }
 
   // navigation scroll to section
   $("#toWork").click(function() {
@@ -15,5 +21,10 @@ $(document).ready(function() {
     $('html, body').animate({
         scrollTop: $("#contact").offset().top
     }, 2000);
+  });
+  $("#toTop").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#top").offset().top
+    }, 1000);
   });
 })

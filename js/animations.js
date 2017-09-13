28e10bbee4174c921117680504dfd4f3e89583e6
cmdeveloped <cmwebdevs@gmail.com>
2017-09-13 14:05:16 -0500

    Add animation to navbar

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-// you're doing it!
-  if ($(window).scrollTop() > 50 ) {
-  $('.nav').css({'position' : 'fixed', 'top' : 0});
-  } else {
-  $('.nav').css({'position' : 'relative', 'top' : 'none'});
-  }
-
   // navigation scroll to section
   $("#toWork").click(function() {
     $('html, body').animate({
@@ -28,3 +21,22 @@ $(document).ready(function() {
     }, 1000);
   });
 })
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 50 ) {
+    console.log('hey');
+  $('.navbar').css({'position' : 'fixed',
+                  'top' : 0,
+                  'background-color' : '#001c33',
+                  'border' : 'none'
+                });
+  $('.navbar ul li').css({'color' : '#fff'});
+  } else {
+  $('.navbar').css({'position' : 'fixed',
+  'top' : 0,
+  'background-color' : '#fff',
+  'border-top' : '8px solid #001c33'});
+  $('.navbar ul li').css({'color' : '#474747'});
+  $('.navbar ul li:hover').css({'color' : '#001c33'})
+  }
+});
